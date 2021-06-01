f39b6ebb9f3b5eca6be6135672e919142c637e70
fer-br-tiger <fernando.gonzalez.261196@gmail.com>
2021-06-01 00:11:35 -0300

    Modify database file

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const mysqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'FeR626Di86',
     database: 'dbInstructores',
     multipleStatements: true
 });
